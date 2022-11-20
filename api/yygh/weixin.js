1260e9176151674af23a9ae653c165c01a571cda
Yiang <1712064922@qq.com>
2022-11-20 15:43:09 +0800

    1、支付服务调用微信接口创建支付记录 2、点击支付按钮生成二维码进行支付 3、修改show页面使用插件显示支付二维码进行支付

--- api/yygh/weixin.js
+++ api/yygh/weixin.js
@@ -1,9 +1,18 @@
 import request from '@/utils/request'
 
 export default {
+
+  //微信登录二维码需要的参数
   getLoginParam() {
     return request({
       url: `/api/user/wx/getLoginParam`,
+      method: 'get'
+    })
+  },
+  //获取支付链接
+  createNative(orderId) {
+    return request({
+      url: `/api/payment/weixin/createNative/${orderId}`,
       method: 'get'
     })
   }
